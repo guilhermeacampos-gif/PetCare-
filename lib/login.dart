c01f591b3fd3a97d8718c26c70e49d06171d165d
Italo dos Santos <italo.doliveira@souunit.com.>
2026-05-19 09:15:04 -0300

    Adicionando textos

--- lib/login.dart
+++ lib/login.dart
@@ -14,14 +14,34 @@ class LoginPage extends StatelessWidget {
             Image.asset('images/logo.png', height: 30, fit: BoxFit.contain),
             SizedBox(width: 10),
             Text(
-              'PET CARE+',
+              'PETCARE+',
               style: TextStyle(
                 color: Theme.of(context).colorScheme.secondary,
                 fontWeight: FontWeight.bold,
-              ),
-            ),
+              )
+            )
+          ]
+        )
+      ),
+      body: Center(
+        child: Column (
+          children: [
+            Text("Login"),
+            Text("Acesse sua conta com seu e-mail e senha"),
+            Column(),
+            Column(),
+            Column(),
+            Row(),
+            SizedBox(),
+            TextButton(onPressed: () {}, child: Text('Login')),
+            Row(
+              children: [
+                Text("Não tem conta?"),
+                Text("Cadastre-se")
+              ]
+            )
           ],
-        ),
+        )
       ),
     );
   }
